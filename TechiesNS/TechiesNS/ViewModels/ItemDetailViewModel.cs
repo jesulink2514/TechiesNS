8dadcbc8bbff47c24c46d39a9c3083bf5d2b06d5
jesulink2514 <jesus.angulo@outlook.com>
2019-02-07 22:24:31 -0500

    Add project files.

---
+++ TechiesNS/TechiesNS/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TechiesNS.Models;
+
+namespace TechiesNS.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
